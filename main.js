0011d316bfa236a203b69af68cba637646e199f2
Oliver Dimes <odime001@gold.ac.uk>
2020-10-20 14:39:17 +0100

    Added renaming folder and basic HTML generation
    Still a long way to go in terms of UI but the back-end JS is almost complete.

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const { app, BrowserWindow,ipcMain } = require('electron')
-// const {fs} = require('fs') //File System 
+const fs = require('fs')
+const path = require('path');
+
+//Global Variables
+let filePath;
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -19,18 +23,50 @@ function createWindow () {
 
 app.whenReady().then(createWindow)
 
-
-ipcMain.on('folderSelection', (event, arg) => {
-  try {
-    let filePath = arg.filePaths[0] //Grabs File Path
-    console.log(filePath);
-  }
-  catch(err)
+function buildHTML(array){
+  filename = "index.html"
+  let header = ""
+  let body = ""
+  for(let i = 0; i < array.length; i++)
   {
-    console.log(err);
+    if(array[i].value == true)
+    {
+      let link = `<script src="${array[i].link}"></script>`
+      console.log("Link Value " + link);
+      header += link;
+    }
+    else
+    {
+      console.log("False")
+    }
   }
-})
 
+  return '<!DOCTYPE html>'
+  + '<html><head>' + header + '</head><body>' + body + '</body></html>';
+}
+
+// https://www.geeksforgeeks.org/node-js-fs-mkdir-method/
+ipcMain.on('folderSelection', (event, arg) => {
+    //Grabs the file path
+    console.log(arg);
+    filePath = path.join(arg.path.filePaths[0],arg.solution);
+
+    //Creates Root Folder
+    fs.mkdir(filePath,function(err){
+      if (err) { 
+        return console.error(err); 
+    } 
+    console.log('Directory created successfully!');
+    console.log(buildHTML(arg.values)); 
+    })
+
+    // fs.mkdir(path.join(path.join(filePath,"Test"),"lib")),function(err){
+    //   if(err){
+    //     return console.error(err);
+    //   }
+    //   console.log("Inner Directory created")
+    // }
+})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
--- renderer.js
+++ renderer.js
@@ -15,12 +15,39 @@ console.log(ipcRenderer.sendSync('synchronous-message', 'ping')) // prints "pong
 // })
 // ipcRenderer.send('asynchronous-message', 'ping')
 
-let button = document.getElementById("createFolder")
+let button = document.getElementById("createFolder");
+
+//List of CDNS
+const p5jsLink = "https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/p5.js"
+const p5jsSoundLink = "https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/addons/p5.sound.js"
+
+function createP5jsLib(name,value,link){
+    let object = {
+        name : name,
+        value : value,
+        link : link
+    };
+    return object
+}
 
 button.addEventListener('click', ()=>{
-    //Promise
-    dialog.showOpenDialog({properties:['openDirectory']}).then(result => ipcRenderer.send("folderSelection",result))
-    // console.log(pathName)
-    // console.log(pathTest)
-    // ipcRenderer.send('folderSelection', pathTest)
+    //Grabbing Values of
+    let values = [];
+    
+    var fileName = document.getElementById("solutionName").value;
+    
+    //Grab values of documents
+    var p5js = document.getElementById("p5js").checked;
+    var p5jsSound = document.getElementById("p5jsSound").checked;
+
+    //Formatting into objects, need to be reworked to allow for 
+    values.push(createP5jsLib("p5js",p5js,p5jsLink));
+    values.push(createP5jsLib("p5jsSound",p5jsSound,p5jsSoundLink));
+    console.log(values);
+
+
+    dialog.showOpenDialog({properties:['openDirectory']}).then(result => {
+        let data = {path : result, solution : fileName, values : values}
+        ipcRenderer.send("folderSelection",data)
+    })
 })
